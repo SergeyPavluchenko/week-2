acf31281041e1517376a74b9bb9f1ee2ead8eea8
SergeyPavluchenko <s.pavluchenko90@gmail.com>
2026-07-01 21:35:14 +0000

    Implemented the requested functions, updated the code, and added filters

--- tasks/task.js
+++ tasks/task.js
@@ -1,71 +1,118 @@
-const tasks = document.querySelector('.task__input')
+const tasksInput = document.querySelector('.task__input')
 const taskBtn = document.querySelector('.task__btm__add')
 const taskList = document.querySelector('.task__ul')
+const message = document.querySelector('.empty_message')
+const filterButton = document.querySelector('.task_filters')
+
+
+let tasks = []
+let currentFilter = "all";
+
+function renderTasks() {
+    taskList.innerHTML = ''
+
+    if (tasks.length === 0) {
+        message.textContent = 'No tasks yet'
+    } else {
+        message.textContent = ''
+    }
+
+    getFilteredTasks().forEach(task => {
+        let li = document.createElement('li')
+        li.textContent = task.text
+        li.dataset.id = task.id
+
+        if (task.completed) {
+            li.classList.add('task__ul__chaked')
+        }
+        let span = document.createElement('span')
+        span.textContent = '\u00d7'
+        li.appendChild(span)
+        taskList.appendChild(li)
+
+    })
+}
+
 
 const handleAddTask = (e) => {
     e.preventDefault()
-    const value = tasks.value
+    const value = tasksInput.value.trim()
     if (value === '') {
-        alert('Enter task')
+        message.textContent = 'No tasks yet'
     } else {
         addTask(value)
-        tasks.value = ''
+        tasksInput.value = ''
+        message.textContent = ''
 
-        saveTask(value)
     }
 }
 
-function saveTask(value) {
-    const arr = JSON.parse(localStorage.getItem('tasks')) || []
-    arr.push(value)
-    localStorage.setItem('tasks', JSON.stringify(arr))
+function saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+
 }
 
-function loadTask() {
-    const arr = JSON.parse(localStorage.getItem('tasks')) || []
-    arr.forEach(task => addTask(task));
+function loadTasks() {
+    tasks = JSON.parse(localStorage.getItem('tasks')) || []
+    renderTasks()
 }
 
 
 function addTask(value) {
+    const newTask = {
+        id: Date.now(),
+        text: value,
+        completed: false
+    }
 
-    let li = document.createElement('li')
-    li.textContent = value
-    taskList.appendChild(li)
-    let span = document.createElement('span')
-    span.textContent = '\u00d7'
-    li.appendChild(span)
+    tasks.push(newTask)
+
+    saveTasks()
+    renderTasks()
 }
 
 const handleToggle = (e) => {
     if (e.target.tagName === 'LI') {
-        e.target.classList.toggle('task__ul__chaked')
-    } else {
-        return
+
+        const id = Number(e.target.dataset.id)
+        const task = tasks.find(t => t.id === id)
+        task.completed = !task.completed
+        saveTasks()
+        renderTasks()
     }
 }
 
 const handleDelete = (e) => {
     if (e.target.tagName === 'SPAN') {
-        const li = e.target.parentElement
-
-        const arrIdx = Array.from(taskList.children).indexOf(li)
-
-        li.remove()
-
-        const arr = JSON.parse(localStorage.getItem('tasks')) || []
-        arr.splice(arrIdx, 1)
-        localStorage.setItem('tasks', JSON.stringify(arr))
-
+        const id = Number(e.target.parentElement.dataset.id)
+        tasks = tasks.filter(t => t.id !== id)
+        saveTasks()
+        renderTasks()
     }
 }
 
 
+const getFilteredTasks = () => {
+    if (currentFilter === 'active') {
+        return tasks.filter(task => !task.completed)
+    } else if (currentFilter === 'completed') {
+        return tasks.filter(task => task.completed)
+    }
+    return tasks
+}
 
-window.addEventListener('load', loadTask)
+const handleFilterBtn = (e) => {
+    if (e.target.tagName === 'BUTTON') {
+        currentFilter = e.target.dataset.filter
+        renderTasks()
+    }
+}
+
+window.addEventListener('load', loadTasks)
 taskList.addEventListener('click', (e) => {
     handleToggle(e)
     handleDelete(e)
 })
 taskBtn.addEventListener('click', handleAddTask);
+filterButton.addEventListener('click', handleFilterBtn)
 
